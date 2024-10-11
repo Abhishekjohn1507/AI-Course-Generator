0e10d7b6f20f7cf0a63df8a29b21602beac989e2
Abhishek John Charan <abhishekjohn.stdnt@srmu.ac.in>
2024-10-11 07:58:28 +0530

    g

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,14 +1,14 @@
 'use client'
-import React, { useState, useEffect } from 'react'
-import SideBar from './_components/SideBar'
-import Header from './_components/Header'
-import { UserCourseListContext } from '../_context/UserCourseListContext';
-import { UserSubscriptionContext } from '../../_context/UserSubsCriptionContext';
+import React, { useState, useEffect } from 'react';
+import SideBar from './_components/SideBar';
+import Header from './_components/Header';
+import { UserCourseListContext } from '../_context/UserCourseListContext';  // Corrected path
+import { UserSubscriptionContext } from '../_context/UserSubsCriptionContext';  // Corrected path
+
 function DashboardLayout({ children }) {
   const [userCourseList, setUserCourseList] = useState([]);
   const [isMounted, setIsMounted] = useState(false);
   const [usersubscription, setUserSubscription] = useState(false);
-  const [updateCourseUsage, setUpdateCourseUsage] = useState(false);
 
   useEffect(() => {
     setIsMounted(true); // Indicate the client-side is mounted
@@ -18,24 +18,20 @@ function DashboardLayout({ children }) {
     return null; // Avoid rendering on the server-side mismatch
   }
 
-
   return (
     <UserCourseListContext.Provider value={{ userCourseList, setUserCourseList }}>
       <UserSubscriptionContext.Provider value={{ usersubscription, setUserSubscription }}>
-        {/* <UpdateCourseUsageContext.Provider value={{ updateCourseUsage, setUpdateCourseUsage }}> */}
-          <div>
-            <div className='md:w-64 hidden md:block'>
-              <SideBar />
-            </div>
-            <div className='md:ml-64 p-5'>
-              <Header />
-              <div className='p-10'>
-                {children}
-              </div>
+        <div>
+          <div className='md:w-64 hidden md:block'>
+            <SideBar />
+          </div>
+          <div className='md:ml-64 p-5'>
+            <Header />
+            <div className='p-10'>
+              {children}
             </div>
           </div>
-        {/* </UpdateCourseUsageContext.Provider> */}
-
+        </div>
       </UserSubscriptionContext.Provider>
     </UserCourseListContext.Provider>
   );
